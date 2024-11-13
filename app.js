a64af5fbac00e54cf6cfdc7807430e8683128bec
Sandi Miftah <sandiseeyou@gmail.com>
2024-11-13 16:19:56 +0700

    adding posts.js coding

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 import express from "express";
 import axios from "axios";
+import bodyParser from "body-parser";
+import { getStoredPosts, storePosts } from "./data/posts.js";
 
 const app = express();
 const PORT = 3000;
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  // Attach CORS headers
+  // Required when using a detached backend (that runs on a different domain)
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 app.get("/", (req, res) => {
   res.sendStatus(200);
