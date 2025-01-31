4a0a5d2c39ed55cf28913b0cb2389fe11adc0ec4
Sandi Miftah <sandiseeyou@gmail.com>
2025-01-31 14:37:50 +0700

    rename folder

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import express from "express";
 import axios from "axios";
 import cors from "cors";
+import bodyParser from "body-parser";
 import dotenv from "dotenv";
-import { fetchData, storeData } from "./data/posts.js";
+import { fetchData, storeData, getMovie } from "./data/posts.js";
 
 const app = express();
 dotenv.config();
@@ -13,10 +14,29 @@ const API_KEY = process.env.API_KEY;
 
 app.use(express.json());
 app.use(cors());
+app.use(bodyParser.json());
+
+let fetchResult;
 
 app.get("/", async (req, res) => {
   const movieList = await fetchData();
   res.json({ blog: movieList });
+});
+
+app.post("/request", async (req, res) => {
+  const idImdb = req.body.movieid;
+
+  try {
+    const result = await axios.get(API_URL, {
+      params: {
+        apiKey: API_KEY,
+        i: idImdb,
+      },
+    });
+    console.log(result);
+  } catch (error) {}
+
+  res.json(200);
 });
 
 app.listen(PORT, () => {
